71c6e686e691dcdd514506249fd6fb145de5e13b
Alice Souza <alicefms@hotmail.com>
2021-11-09 12:23:36 -0300

    Após implementar metodos do jpa, vai implementar alguns atributos pelo DTO

--- src/main/java/com/controle_despesa/model/entity/Categoria.java
+++ src/main/java/com/controle_despesa/model/entity/Categoria.java
@@ -5,13 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 //a dependência LOMBOK facilita fazer os gets, sets e construtores padrão:
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //faz com q gere automatico a ID. Se eu colocar uma Id, o programa ignora .
     private Long id;
     private String descricao;
+    private String dataCadastro;
 }
